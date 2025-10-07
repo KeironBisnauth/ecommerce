a6fca06368da4f672726b32efc4d90f379208771
Keiron Bisnauth <alexbisnauth35@gmail.com>
2025-10-06 20:13:38 -0400

    Worked on store page and store functionality
    Implemented store page and shop page

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Helpers\ProductCollectionHelper;
+use Illuminate\Contracts\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Collection;
@@ -11,6 +12,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Product extends Model
 {
     use HasFactory;
+
 
 
     /**
@@ -34,12 +36,42 @@ class Product extends Model
      */
 
 
+    public function scopeWithPrices(Builder $query, array $group_ids = [1])
+    {
+
+        $query->where('products.id', '>', 0);
+    }
+
+
+    public function scopeSingleProdcut(Builder $query, int $id)
+    {
+        $query->where('products.id', $id);
+    }
     /**
      *  =============== FUNCTIONS  ===============
      */
+    public function getImage()
+    {
+        return asset('storage' . $this->image_path . $this->image_name);
+    }
 
+    public function getPrice()
+    {
+        return $this->price;
+    }
 
+    public function getStockPrice()
+    {
+        return $this->price;
+    }
 
+    public function getCartQuantityPrice()
+    {
+        return $this->getPrice() * $this->pivot->quantity;
+    }
 
-
+    public function getLink()
+    {
+        return route('shop.details', ['id' => $this->id]);
+    }
 }
